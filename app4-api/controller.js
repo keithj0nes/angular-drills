443f03a45be91268d9fecc59f05b615395d97dbf
keith <keith@raincityambience.com>
2016-10-30 22:47:43 -0600

    finish app4

--- app4-api/controller.js
+++ app4-api/controller.js
@@ -1,6 +1,9 @@
 angular.module("apiApp").controller("mainCtrl", function($scope, mainSvc){
 
   mainSvc.getShipInfo().then(function(starShips){
+    // if(mainSvc.getShipInfo(reponse.data.results.cost_in_credits) === "unknown"){
+    //   return ""
+    // }
     $scope.starShips = starShips;
   })
 
